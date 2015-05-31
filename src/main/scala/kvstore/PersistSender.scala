31e6fe7b6df986702af31c4006bfed565a9d98a3
Tim Kwei <taichiski@gmail.com>
2015-05-31 11:28:27 -0400

    step4 persist retry
    add persist retry

--- src/main/scala/kvstore/PersistSender.scala
+++ src/main/scala/kvstore/PersistSender.scala
@@ -3,29 +3,36 @@ package kvstore
 import akka.actor.Actor
 import akka.actor.ActorRef
 import akka.actor.Props
+import scala.concurrent.Future
 
 object PersistSender {
   case class SendPersist(key: String, valueOption: Option[String], seq: Long)
   case class PersistSent(key: String, seq: Long)
-  
+
   def props(persistActor: ActorRef): Props = Props(new PersistSender(persistActor))
 }
 class PersistSender(val persistActor: ActorRef) extends Actor {
   import PersistSender._
   import Persistence._
   import context.dispatcher
-  
-  var persistMap = Map.empty[Long,ActorRef]
-  
+
+  var persistMap = Map.empty[Long, ActorRef]
+
   def receive: Receive = {
-    case SendPersist(key,valueOption,seq) => {
-       persistMap += (seq -> sender)
-       persistActor ! Persist(key,valueOption,seq)
+    case SendPersist(key, valueOption, seq) => {
+      persistMap += (seq -> sender)
+      Future[Unit] {
+        while (persistMap.keySet.contains(seq)) {
+          persistActor ! Persist(key, valueOption, seq)
+          Thread.sleep(100)
+        }
+      }
+
     }
-    case Persisted(key,seq) => {
+    case Persisted(key, seq) => {
       val psnd = persistMap(seq)
       persistMap -= seq
-      psnd ! PersistSent(key,seq)
+      psnd ! PersistSent(key, seq)
     }
     case _ =>
   }
